c6a8193a74b70361794450f0fcf833895e547f8e
Nikita <marknik139@gmail.com>
2022-04-08 23:21:17 +0300

    window centered

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from tkinter import Tk, Frame, BOTH
 
 class Example(Frame):
+
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")
         self.parent = parent
-        self.initUI()
-
-    def initUI(self):
-        self.parent.title("Simple")
+        self.parent.title("Centered")
         self.pack(fill=BOTH, expand=1)
+        self.centerWindow()
+
+    def centerWindow(self):
+
+        w = 290
+        h = 150
+
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+
+        x = (sw - w)/2
+        y = (sh -h)/2
+
+        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
 def main():
+
     root = Tk()
     root.geometry("250x150+300+300")
     app = Example(root)
