d458783b58dae5fcf42ac669816b742f1224a2e0
Nikita <marknik139@gmail.com>
2022-04-09 21:26:07 +0300

    2 buttons

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-from PIL import Image, ImageTk
-from tkinter import Tk, BOTH
-from tkinter.ttk import Frame, Label, Style
+from tkinter import Tk, RIGHT, BOTH, RAISED
+from tkinter.ttk import Frame, Button, Style
 
 class Example(Frame):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
+        self.master.title("Buttons")
+        self.style = Style()
+        self.style.theme_use("default")
 
-        self.master.title("Absolute positioning")
-        self.pack(fill=BOTH, expand=1)
+        frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame.pack(fill=BOTH, expand=True)
 
-        Style().configure("TFrame", background="#333")
+        self.pack(fill=BOTH, expand=True)
 
-        bard = Image.open("bardejov.jpg")
-        bardejov = ImageTk.PhotoImage(bard)
-        label1 = Label(self, image=bardejov)
-        label1.image = bardejov
-        label1.place(x=20, y=20)
-
-        rot = Image.open("rotunda.jpg")
-        rotunda = ImageTk.PhotoImage(rot)
-        label2 = Label(self, image=rotunda)
-        label2.image = rotunda
-        label2.place(x=40, y=160)
-
-        minc = Image.open("mincol.jpg")
-        mincol = ImageTk.PhotoImage(minc)
-        label3 = Label(self, image=mincol)
-        label3.image = mincol
-        label3.place(x=170, y=50)
+        closeButton = Button(self, text="close")
+        closeButton.pack(side=RIGHT, padx=5, pady=5)
+        okButton = Button(self, text="ok")
+        okButton.pack(side=RIGHT)
 
 
 def main():
